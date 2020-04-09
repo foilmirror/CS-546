dcd138aef77e43785150b33c85ecf953416b2234
foilmirror <spencerjonas@gmail.com>
2020-04-08 21:56:45 -0400

    One last update/fix

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,10 @@
-<<<<<<< HEAD
 const mainRoutes = require('./main');
 const postRoutes = require('./posts');
 const searchRoutes = require('./search');
-=======
-const postRoutes = require('./posts');
->>>>>>> 3502ad965c7b77bd338e42fbdcf64f628fe28889
 const userRoutes = require('./users');
 const path = require('path');
 
 const constructorMethod = (app) => {
-<<<<<<< HEAD
     app.use('/', mainRoutes);
     app.use('/post', postRoutes);
 	app.use('/search', searchRoutes);
@@ -24,15 +19,3 @@ const constructorMethod = (app) => {
 };
 
 module.exports = constructorMethod;
-=======
-	app.use('/posts', postRoutes);
-	app.use('/users', userRoutes);
-	
-
-	app.use('*', (req, res) => {
-		res.redirect('/posts');
-	});
-};
-
-module.exports = constructorMethod;
->>>>>>> 3502ad965c7b77bd338e42fbdcf64f628fe28889
