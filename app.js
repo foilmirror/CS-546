e61235114e4d5e36d5e16c64a43e5729298dedb8
foilmirror <spencerjonas@gmail.com>
2020-05-11 01:19:37 -0400

    Some setup for ajax

--- app.js
+++ app.js
@@ -2,13 +2,21 @@ const express = require('express');
 const session = require('express-session');
 const app = express();
 const static = express.static(__dirname + '/public');
-
 const configRoutes = require('./routes');
 const exphbs = require('express-handlebars');
+
+const rewriteUnsupportedBrowserMethods = (req, res, next) => {
+	if (req.body && req.body._method) {
+	  req.method = req.body._method;
+	  delete req.body._method;
+	}
+	next();
+};
 
 app.use('/public', static);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(rewriteUnsupportedBrowserMethods);
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
@@ -21,24 +29,15 @@ app.use(session({
     resave: false,
     saveUninitialized: true
 }))
-  
+
 app.use(async (req, res, next) => {
 	if (!req.session.user) {
 		console.log("[" + new Date().toUTCString() + "]: " + req.method + " to " + req.originalUrl + " (Not logged in)");
-		next();
 	}
 	else {
 		console.log("[" + new Date().toUTCString() + "]: " + req.method + " to " + req.originalUrl + " (Logged in as " + req.session.user._id + ")");
-		next();
 	}
-});
-
-app.use('/', async (req, res, next) => {
-	if (req.url == '/' && !req.session.user) {
-    	return res.redirect('/users/login');
-	} else {
-    next();
-  }
+	next();
 });
 
  
