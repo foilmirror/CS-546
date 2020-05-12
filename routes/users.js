239cda23a800e68b5f46b1dbe5c483f729fa078f
Paul Chua <pchua@stevens.edu>
2020-05-12 18:19:12 -0400

    Probably overdid it, but xss added to user routes

--- routes/users.js
+++ routes/users.js
@@ -58,7 +58,7 @@ router.get('/:id', async (req, res) => {
   
     }
     if(req.session.user){
-      if(req.session.user._id != c_user._id){
+      if(xss(req.session.user._id) != c_user._id){
           diff = true;
       }
 
@@ -67,7 +67,7 @@ router.get('/:id', async (req, res) => {
       }
       if(c_user.friends){
       for(let x = 0 ; x < c_user.friends.length; x++){
-        if(req.session.user._id == c_user.friends[x].id){
+        if(xss(req.session.user._id) == c_user.friends[x].id){
           f = true;
           diff = false;
           break;
@@ -99,13 +99,13 @@ router.post("/login", async (req, res) => {
       return;
   }
   
-  data.userName = data.userName.toLowerCase();
+  data.userName = xss(data.userName.toLowerCase());
   try{
       let success = false;
       const users  = await userData.getAllUsers();
-      let user = users.find(u => u.userName.toLowerCase() == data.userName);
+      let user = users.find(u => u.userName.toLowerCase() == xss(data.userName));
       if(user){
-          success = await bcrypt.compare(data.password, user.Password);
+          success = await bcrypt.compare(xss(data.password), user.Password);
   
           if(success=== true){
               //Worked~
@@ -142,10 +142,10 @@ router.post('/', async (req, res) => {
     res.status(400).json({error: 'You must provide a user name'});
     return;
   }
-  userInfo.userName = userInfo.userName.toLowerCase();
+  userInfo.userName = xss(userInfo.userName.toLowerCase());
   const users  = await userData.getAllUsers();
 
-  let e = users.find(u => u.userName.toLowerCase() == userInfo.userName);
+  let e = users.find(u => u.userName.toLowerCase() == xss(userInfo.userName));
 
   if(e){
     res.render('users/register', {error: true, etext: "Username is already taken"});
@@ -157,7 +157,7 @@ router.post('/', async (req, res) => {
     return;
   }
 
-  userInfo.Email = userInfo.Email.toLowerCase();
+  userInfo.Email = xss(userInfo.Email.toLowerCase());
   let f = users.find(u => u.Email.toLowerCase() == userInfo.Email);
 
   if(f){
@@ -196,8 +196,8 @@ router.post('/', async (req, res) => {
   }
 
   try {
-    const h = await bcrypt.hash(userInfo.Password,saltRounds);
-    const newUser = await userData.addUser(userInfo.userName,userInfo.Email,userInfo.profilePhoto,userInfo.Gender,userInfo.City,userInfo.State,userInfo.Age,h);
+    const h = await bcrypt.hash(xss(userInfo.Password),saltRounds);
+    const newUser = await userData.addUser(xss(userInfo.userName),xss(userInfo.Email),xss(userInfo.profilePhoto),xss(userInfo.Gender),xss(userInfo.City),xss(userInfo.State),xss(userInfo.Age),xss(h));
     res.render('users/login', {});
   } catch (e) {
     res.sendStatus(500);
@@ -207,7 +207,7 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   
   let userInfo = req.body;
-  let c_user = await userData.getUserById(req.params.id);
+  let c_user = await userData.getUserById(xss(req.params.id));
     let diff = false;
     let f = false;
     let you = false;
@@ -258,10 +258,10 @@ router.put('/:id', async (req, res) => {
     res.status(400).json({error: 'You must provide a user name'});
     return;
   }
-  userInfo.userName = userInfo.userName.toLowerCase();
+  userInfo.userName = xss(userInfo.userName.toLowerCase());
   const users  = await userData.getAllUsers();
 
-  let e = users.find(u => u.userName.toLowerCase() == userInfo.userName);
+  let e = users.find(u => u.userName.toLowerCase() == xss(userInfo.userName));
   
   if(e){
   if(e._id != req.params.id){
@@ -277,7 +277,7 @@ router.put('/:id', async (req, res) => {
   }
 
   userInfo.Email = userInfo.Email.toLowerCase();
-  let em = users.find(u => u.Email.toLowerCase() == userInfo.Email);
+  let em = users.find(u => u.Email.toLowerCase() == xss(userInfo.Email));
   if(em){
   if(em._id != req.params.id){
     res.render('users/user', {user: c_user, nyou: diff, amigos: amigos, posts: posta, i: req.session.user, friend: f, you:you, error:true, etext: "email taken"});
@@ -326,14 +326,14 @@ router.put('/:id', async (req, res) => {
     await userData.getUserById(req.params.id);
     //If the updated thing was supposed to be friends, it will be here (happens when a person adds someone as a friend)
     if(userInfo.friends && req.session.user){
-      await userData.addFriendtoUser(req.session.user._id,req.params.id);
-      await userData.updateUser(req.session.user._id, req.session.user);
-      await userData.addFriendtoUser(req.params.id,userInfo.friends);
-      await userData.updateUser(req.params.id, userInfo);
-      res.redirect('/users/' + req.params.id)
+      await userData.addFriendtoUser(xss(req.session.user._id),xss(req.params.id));
+      await userData.updateUser(xss(req.session.user._id), xss(req.session.user));
+      await userData.addFriendtoUser(xss(req.params.id),xss(userInfo.friends));
+      await userData.updateUser(xss(req.params.id), xss(userInfo));
+      res.redirect('/users/' + xss(req.params.id))
     }
-    const updatedUser = await userData.updateUser(req.params.id, userInfo);
-    res.redirect('/users/' + req.params.id);
+    const updatedUser = await userData.updateUser(xss(req.params.id), xss(userInfo));
+    res.redirect('/users/' + xss(req.params.id));
   } catch (e) {
     res.sendStatus(500);
   }
@@ -341,7 +341,7 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   try {
-    await userData.getUserById(req.params.id);
+    await userData.getUserById(xss(req.params.id));
   } catch (e) {
     res.status(404).json({error: 'User not found'});
     return;
@@ -351,14 +351,14 @@ router.delete('/:id', async (req, res) => {
     //This will handle deleting users from friend lists (when a button is pressed on the actual site)
     //This is because removing friends is a user to user thing so idk where else this would go lol
     if(req.body.userID){
-      await userData.removeFriend(req.params.id,req.session.user._id);
-      await userData.removeFriend(req.session.user._id,req.params.id);
-      res.redirect('/users/' + req.params.id);
+      await userData.removeFriend(xss(req.params.id),xss(req.session.user._id));
+      await userData.removeFriend(xss(req.session.user._id),xss(req.params.id));
+      res.redirect('/users/' + xss(req.params.id));
   
     }
     //A normal delete request will have no body
     else{
-    await userData.removeUser(req.params.id);
+    await userData.removeUser(xss(req.params.id));
     res.redirect('/users/');
 
     }
